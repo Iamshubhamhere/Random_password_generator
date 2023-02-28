9c7b39b8b8c34cadb8ed1b3a2a2a3b40b9530bdd
Shubham <shubham11729@gmail.com>
2023-02-27 20:12:46 -0600

    alphabet generator

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function alphabetGenerator() {
+    const alphabet = [
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+       "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+    let out = "";
+    for (let i = 0; i < 4; i++) {
+      out = out + alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    return out;
+  }
